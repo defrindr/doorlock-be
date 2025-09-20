5dd4347e82540fa6b1a64f9e35720f535aaae4e1
defrindr <defrindr@gmail.com>
2025-09-21 01:17:49 +0700

    feat(migrations): add gate_identifier column to gates table with unique index

---
+++ src/db/migrations/1758182403104-add-gate-identifier-to-gates.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AddColumnGateIdentifierTable1758182403104
+  implements MigrationInterface
+{
+  name = 'AddColumnGateIdentifierTable1758182403104';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Add the new column
+    await queryRunner.addColumn(
+      'gates',
+      new TableColumn({
+        name: 'gate_identifier',
+        type: 'varchar',
+        length: '50',
+        isNullable: true, // or false, depending on your need
+      }),
+    );
+
+    // Create index for the new column
+    await queryRunner.query(`
+      CREATE UNIQUE INDEX "UQ_gates_gate_identifier"
+      ON "gates" ("gate_identifier")
+      WHERE "gate_identifier" IS NOT NULL
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropIndex('gates', 'UQ_gates_gate_identifier');
+    await queryRunner.dropColumn('gate_identifier', 'gate_identifier');
+  }
+}
